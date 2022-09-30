bbff78ff24a3955995c8fab171b7ffa6805d9771
Leli1024 <33652168+Leli1024@users.noreply.github.com>
2022-09-30 15:53:36 +0200

    Update modeling_bart.py

--- src/transformers/models/bart/modeling_bart.py
+++ src/transformers/models/bart/modeling_bart.py
@@ -843,7 +843,7 @@ class BartEncoder(BartPretrainedModel):
         hidden_states = self.layernorm_embedding(hidden_states)
         hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
         
-        blurred_hidden_states = blurred_inputs_embeds + embed_pos
+        blurred_hidden_states = blurred_inputs_embeds + self.embed_pos(blurred_input)
         blurred_hidden_states = self.layernorm_embedding(blurred_hidden_states)
         blurred_hidden_states = nn.functional.dropout(blurred_hidden_states, p=self.dropout, training=self.training)
 
